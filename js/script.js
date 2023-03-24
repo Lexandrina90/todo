dfb8669291287a1a04d7f12b3b8326317ad25ac3
Lexandrina90 <lexandrina90@gmail.com>
2023-03-24 10:38:26 +0300

    without CSS (JS not done till end)

---
+++ js/script.js
@@ -0,0 +1,58 @@
+const todoForm = document.querySelector('.todo-form');
+const todoList = document.querySelector('.todo-list')
+let tasks = [];
+
+const input = document.querySelector('input');
+
+todoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+ 
+  const inputValue = input.value;
+  // console.log(inputValue);
+  
+  if (inputValue != "") {
+    const task = {
+       id: new Date().getTime(),
+       name: inputValue,
+    };
+    tasks.push(task);
+    // console.log(tasks);
+    createTask(task);
+
+    todoForm.reset();
+  }
+  input.focus();
+});
+
+const createTask  = function (task) {
+  const taskItem = document.createElement("li");
+  taskItem.setAttribute("id",task.id);
+  const taskHtml = `
+  <div class = "list-container">
+  <input type="checkbox" id="${task.id}">
+  <label for="${task.name}"></label>
+  <span>${task.name}</span>
+  </div>
+  <button class="edit-btn">Edit</button>
+  <button class="delete-btn">Delete</button>
+  `;
+  taskItem.innerHTML = taskHtml;
+  todoList.appendChild(taskItem); 
+}
+
+todoList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("edit-btn")) {
+        const taskId = e.target.closest("li").id;
+        const taskSpan = document.getElementById(taskId).querySelector("span");
+        // console.log(taskSpan.textContent);
+        if(e.target.textContent == 'Edit') {
+            e.target.textContent= "Save"
+        } 
+       
+      } else if (e.target.classList.contains("delete-btn")) {
+        const taskId = e.target.closest("li").id;
+        // console.log(taskId);
+        document.getElementById(taskId).remove();
+      }
+});
+
