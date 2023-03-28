69129a347f2029ba985428fc3f06776bf9ce523a
Lexandrina90 <lexandrina90@gmail.com>
2023-03-28 15:16:23 +0300

    add isVisible, deleted filteredTasks

--- js/script.js
+++ js/script.js
@@ -3,36 +3,35 @@ const todoList = document.querySelector('.todo-list');
 const input = document.querySelector('input');
 
 const tasks = [];
-let filteredTasks = [];
+
 const filterKeys = [
   { value: 'all', selected: true },
   { value: 'active', selected: false },
   { value: 'done', selected: false }
 ];
 
-
 const addTask = function (task) {
   tasks.push({
     text: task,
-    isDone: false
+    isDone: false,
+    isVisible: true
   });
   input.value = '';
   filterTasks();
 }
-
 
 const filterTasks = function () {
   for (let i = 0; i < filterKeys.length; i++) {
     if (filterKeys[i].selected === true) {
       switch (filterKeys[i].value) {
         case 'all':
-          filteredTasks = tasks.slice();
+          tasks.forEach(task => task.isVisible = true)
           break;
         case 'active':
-          filteredTasks = tasks.filter(task => !task.isDone);
+          tasks.forEach(task => task.isVisible = !task.isDone);
           break;
         case 'done':
-          filteredTasks = tasks.filter(task => task.isDone);
+          tasks.forEach(task => task.isVisible = task.isDone);
           break;
       }
       displayTasks();
@@ -42,75 +41,66 @@ const filterTasks = function () {
 
 const displayTasks = function () {
   todoList.innerHTML = '';
-
   tasks.forEach((task, index) => {
-    if(filteredTasks.includes(task)) {
-    const taskItem = document.createElement('li');
+    if (task.isVisible) {
+      const taskItem = document.createElement('li');
+      const listWrap = document.createElement('div');
+      taskItem.appendChild(listWrap);
 
-    const listWrap = document.createElement('div');
-    taskItem.appendChild(listWrap);
-
-    //checkbox
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.checked = task.isDone;
-    checkbox.addEventListener('click', () => {
-      const isDone = task.isDone;
-      if (!isDone) {
-        task.isDone = true;
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.checked = task.isDone;
+      checkbox.addEventListener('click', () => {
+        const isDone = task.isDone;
+        if (!isDone) {
+          task.isDone = true;
+          filterTasks();
+        } else {
+          task.isDone = false;
+        }
         filterTasks();
-      } else {
-        task.isDone = false;
-      }
-    });
-    listWrap.appendChild(checkbox);
+      });
+      listWrap.appendChild(checkbox);
 
-    //taskText
-    const taskText = document.createElement('span');
-    taskText.textContent = task.text;
-    listWrap.appendChild(taskText);
+      const taskText = document.createElement('span');
+      taskText.textContent = task.text;
+      listWrap.appendChild(taskText);
 
 
-    //BTN
-    const listBtn = document.createElement('div');
-    listBtn.className = 'list-btn';
+      const listBtn = document.createElement('div');
+      listBtn.className = 'list-btn';
 
-    const editBtn = document.createElement('button');
-    editBtn.textContent = 'Edit';
-    editBtn.className = 'edit-btn';
-    editBtn.addEventListener('click', () => {
-      const isEditing = taskText.isContentEditable;
-      if (!isEditing) {
-        editBtn.textContent = 'Save';
-        taskText.contentEditable = true;
-        taskText.focus();
-      } else {
-        editBtn.textContent = 'Edit';
-        taskText.contentEditable = false;
-        tasks[index].text = taskText.textContent;
+      const editBtn = document.createElement('button');
+      editBtn.textContent = 'Edit';
+      editBtn.className = 'edit-btn';
+      editBtn.addEventListener('click', () => {
+        const isEditing = taskText.isContentEditable;
+        if (!isEditing) {
+          editBtn.textContent = 'Save';
+          taskText.contentEditable = true;
+          taskText.focus();
+        } else {
+          editBtn.textContent = 'Edit';
+          taskText.contentEditable = false;
+          tasks[index].text = taskText.textContent;
+          filterTasks();
+        }
+      })
+      listBtn.appendChild(editBtn);
+
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = 'Delete';
+      deleteBtn.className = 'delete-btn';
+      deleteBtn.addEventListener('click', () => {
+        tasks.splice(index, 1);
         filterTasks();
-      }
-    })
-    listBtn.appendChild(editBtn);
+      })
+      listBtn.appendChild(deleteBtn);
 
-    const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'Delete';
-    deleteBtn.className = 'delete-btn';
-    deleteBtn.addEventListener('click',() => {
-      tasks.splice(index,1);
-      filterTasks();
-    } )
-
-
-    listBtn.appendChild(deleteBtn);
-
-
-    taskItem.appendChild(listBtn);
-
-    todoList.appendChild(taskItem);
-  }
+      taskItem.appendChild(listBtn);
+      todoList.appendChild(taskItem);
+    }
   })
-
 }
 
 
